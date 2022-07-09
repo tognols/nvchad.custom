cc75f0b2538c568f292584034b8b0392079203a1
Matteo Tognolo <matteo@tognolo.it>
2022-07-09 09:37:42 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# NvChad custom folder
+
+NvChad config for Windows, Linux and MacOs
+
+Distant.nvim not working on Windows for some reason
---
+++ plugins/init.lua
@@ -0,0 +1,34 @@
+return {
+  ['nvim-neorg/neorg'] = {
+    config = function ()
+     require('neorg').setup {
+       load = {
+         ["core.defaults"] = {},
+         --["core.norg.dirman"] = {
+           --workspaces = {
+             --home = "~/notes/home",
+           --}
+         --}
+       }
+     }
+    end
+  },
+  ['max397574/better-escape.nvim'] = {
+    config = function()
+    require("better_escape").setup()
+    end
+  },
+  ['nvim-orgmode/orgmode'] = {
+    config = function()
+     require("orgmode").setup()
+     require("orgmode").setup_ts_grammar()
+    end
+  },
+  ['chipsenkbeil/distant.nvim'] = {
+    config = function ()
+      require("distant").setup {
+        ['*'] = require('distant.settings').chip_default()
+      }
+    end
+  }
+}
